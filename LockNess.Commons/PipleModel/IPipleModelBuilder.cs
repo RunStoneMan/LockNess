0efeb2a8497b3f3e8f2fcc9a6928fb9f81085cf5
huhan <hu_han_2007@hotmail.com>
2021-12-10 12:26:23 +0800

    Add project files.

---
+++ LockNess.Commons/PipleModel/IPipleModelBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockNess.Commons.PipleModel
+{
+    public interface IPipleModelBuilder
+    {
+        IPipleModelBuilder Use(Func<RequestDelegate, RequestDelegate> middleware);
+        RequestDelegate Build();
+    }
+}
---
+++ LockNess.Commons/PipleModel/PipleModelBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockNess.Commons.PipleModel
+{
+    public class PipleModelBuilder: IPipleModelBuilder
+    {
+        private readonly IList<Func<RequestDelegate, RequestDelegate>> _components = new List<Func<RequestDelegate, RequestDelegate>>();
+        public IPipleModelBuilder Use(Func<RequestDelegate, RequestDelegate> middleware)
+        {
+            _components.Add(middleware);
+            return this;
+        }
+
+        public RequestDelegate Build()
+        {
+            RequestDelegate app = context =>
+            {
+                return Task.CompletedTask;
+            };
+            foreach (var component in _components.Reverse())
+            {
+                app = component(app);
+            }
+            return app;
+        }
+    }
+}
